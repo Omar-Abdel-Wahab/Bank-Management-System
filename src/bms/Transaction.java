bcb3852c00f6cbee1e5ef3eda27e918002bf5fa2
DELL <DELL@ECC>
2018-08-15 19:59:02 +0200

    Commit 5

--- src/bms/Transaction.java
+++ src/bms/Transaction.java
@@ -74,25 +74,26 @@ public class Transaction {
                 && t.transferredTo.equals(transferredTo) && t.type.equals(type);
     }
 
-    public void withdraw(int amount) {
-        if (defaultAccount.isOpen()) {
-            double balance = defaultAccount.getBalance();
+    public void withdraw(int amount) throws InsufficientBalance, NegativeMoney {
+        int balance = defaultAccount.getBalance();
+        if (amount < 0) {
+            throw new NegativeMoney("Can't pull negative money");
+        }
+        if (defaultAccount.isOpen() && amount <= balance) {
             defaultAccount.setBalance(balance - amount);
-            //System.out.println("Done. Please receive your money through the ATM slot.");
         } else {
-            //System.out.println("Sorry, your operation can't be executed. Account is 
-            //closed.");
+            throw new InsufficientBalance("Action can't be performed. You're trying to"
+                    + " pull an amount of money greater than your balance.");
         }
     }
 
-    public void deposit(int amount) {
+    public void deposit(int amount) throws NegativeMoney {
+        if (amount < 0) {
+            throw new NegativeMoney("Can't put negative money");
+        }
         if (defaultAccount.isOpen()) {
-            double balance = defaultAccount.getBalance();
+            int balance = defaultAccount.getBalance();
             defaultAccount.setBalance(balance + amount);
-            //System.out.println("Done. Entered money is added to your account.");
-        } else {
-            //System.out.println("Sorry, your operation can't be executed. Account is 
-            //closed.");
         }
     }
 
@@ -101,9 +102,9 @@ public class Transaction {
     }
 
     public void moneyTransfer(int amount) {
-        double balance1 = transferredFrom.getBalance();
+        int balance1 = transferredFrom.getBalance();
         if (transferredFrom.isOpen() && transferredTo.isOpen() && amount <= balance1) {
-            double balance2 = transferredTo.getBalance();
+            int balance2 = transferredTo.getBalance();
             transferredFrom.setBalance(balance1 - amount);
             transferredTo.setBalance(balance2 + amount);
             //System.out.println("Done. Money transferred to the desired account.");
@@ -113,4 +114,29 @@ public class Transaction {
         }
     }
 
+    public void requestLoan() throws LoanException {
+        if (defaultAccount.isOnLoan()) {
+            throw new LoanException("You're already on loan");
+        } else if (defaultAccount.getBalance() <= 1000 && defaultAccount.isOpen()) {
+            defaultAccount.setBalance(defaultAccount.getBalance() + 20000);
+            defaultAccount.setOnLoan(true);
+        } else if (defaultAccount.getBalance() > 1000) {
+            throw new LoanException("Your balance is greater than 1000. Can't request loan");
+        } else {
+            throw new LoanException("Account is closed");
+        }
+    }
+
+    public void payLoan() throws LoanException {
+        if (!defaultAccount.isOnLoan()) {
+            throw new LoanException("You're not on loan");
+        } else if (defaultAccount.getBalance() >= 20000 && defaultAccount.isOpen()) {
+            defaultAccount.setBalance(defaultAccount.getBalance() - 20000);
+            defaultAccount.setOnLoan(false);
+        } else if (defaultAccount.getBalance() < 20000) {
+            throw new LoanException("Can't pay loan due to insufficient balance");
+        } else {
+            throw new LoanException("Account is closed");
+        }
+    }
 }
